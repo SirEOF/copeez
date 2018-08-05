5a51fc683aa9f939b6270778dbb454d6c4e94958
Alex Levinson <gen0cide.threats@gmail.com>
2018-08-05 10:20:29 -0700

    these libs suck

--- copeez.go
+++ copeez.go
@@ -7,7 +7,15 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/cheggaaa/pb"
+	"github.com/fatih/color"
+	"gopkg.in/cheggaaa/pb.v2"
+)
+
+var (
+	titleColor    = color.New(color.FgHiWhite, color.Bold)
+	labelColor    = color.New(color.FgHiGreen)
+	labelaltColor = color.New(color.FgHiWhite)
+	template      = `{{ string . "title" }}{{ counters . "%s/%s" "%s/?" | yellow }} {{ bar . (white "[") (green "=") (green ">") (red "--") (white "]") }} {{ percent . | yellow }} {{ etime . | cyan }}`
 )
 
 // CopyFile copies a file from source to dest
@@ -17,16 +25,21 @@ func CopyFile(source string, dest string) error {
 		return fmt.Errorf("could not access file %s: %v", source, err)
 	}
 
-	bar := pb.New(int(fi.Size())).SetUnits(pb.U_BYTES)
-	fmt.Printf("### File Copy Task\n#\tSRC: %s\n#\tDST: %s\n", source, dest)
-	bar.Start()
+	fmt.Fprintf(color.Output, "Copying File:\nsrc = %s\ndst = %s\n", color.HiRedString(source), color.HiGreenString(dest))
+	name := "copy"
+	b := pb.New64(fi.Size())
+	b.SetTemplate(pb.ProgressBarTemplate(template))
+	b.SetWriter(color.Output)
+	b.Set("prefix", name)
+	title := fmt.Sprintf("%s%s%s %s ", titleColor.Sprintf("  STATUS"), labelaltColor.Sprintf(":"), labelColor.Sprintf(name), labelaltColor.Sprintf(">>"))
+	b.Set("title", title)
 
 	src, err := os.Open(source)
 	if err != nil {
 		return err
 	}
 
-	reader := bar.NewProxyReader(src)
+	reader := b.NewProxyReader(src)
 
 	defer src.Close()
 
@@ -40,6 +53,9 @@ func CopyFile(source string, dest string) error {
 		return err
 	}
 	defer dst.Close()
+
+	b.Start()
+	defer b.Finish()
 
 	_, err = io.Copy(dst, reader)
 	if err != nil {
